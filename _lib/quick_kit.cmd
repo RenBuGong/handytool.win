7a3484dbabd3b5c237ec28c89703679b336a831a
orwithout <he.haifeng@hotmail.com>
2025-04-14 15:21:15 +0800

    修订bug、细节优化

--- _lib/quick_kit.cmd
+++ _lib/quick_kit.cmd
@@ -44,11 +44,13 @@ rem ----------------------------------------------------------------------------
 rem 本地快捷操作符.
 rem -----------------------------------------------------------------------------
 if "%1"=="___local___" if not "%2"=="" if "%3"=="" (
+    echo --in if 1--
     start explorer.exe "%2"
     exit /b 0
 )
 
 if "%1"=="___local___" if not "%2"=="" if "%3"=="v" (
+    echo --in if 2--
     code "%2"
     exit /b 0
 )
@@ -118,18 +120,22 @@ rem ----------------------------------------------------------------------------
 rem 远程操作符: 添加/删除 key
 rem -----------------------------------------------------------------------------
 if "%arg5%"=="__add.key__" if "%arg6%"=="" (
+    echo --in if 3--
     PowerShell -NoProfile -ExecutionPolicy Bypass -File "%~dp0quick_kit.ps1" -Port "%port%" -RemoteHost "%host%" -RemoteUser "%defUser%" -SshKeyPath "%sshKeyPath%" -PubKeyPath "%arg6%" -Action "add"
     exit /b
 )
 if "%arg5%"=="__remove.key__" if "%arg6%"=="" (
+    echo --in if 4--
     PowerShell -NoProfile -ExecutionPolicy Bypass -File "%~dp0quick_kit.ps1" -Port "%port%" -RemoteHost "%host%" -RemoteUser "%defUser%" -SshKeyPath "%sshKeyPath%" -PubKeyPath "%arg6%" -Action "remove"
     exit /b
 )
 if not "%arg5%"=="" if "%arg6%"=="__add.key__" (
+    echo --in if 5--
     PowerShell -NoProfile -ExecutionPolicy Bypass -File "%~dp0quick_kit.ps1" -Port "%port%" -RemoteHost "%host%" -RemoteUser "%arg5%"    -SshKeyPath "%sshKeyPath%" -PubKeyPath "%arg7%" -Action "add"
     exit /b
 )
 if not "%arg5%"=="" if "%arg6%"=="__remove.key__" (
+    echo --in if 6--
     PowerShell -NoProfile -ExecutionPolicy Bypass -File "%~dp0quick_kit.ps1" -Port "%port%" -RemoteHost "%host%" -RemoteUser "%arg5%"    -SshKeyPath "%sshKeyPath%" -PubKeyPath "%arg7%" -Action "remove"
     exit /b
 )
@@ -141,8 +147,9 @@ rem ----------------------------------------------------------------------------
 
 rem 如果没有给 arg5~7, 就是最简单的 SSH 登录(用默认用户 + 指定Key)
 if "%arg5%"=="" if "%arg6%"=="" if "%arg7%"=="" (
-    echo "SSH: %defUser%@%host% -p %port% (key=%sshKeyPath%)"
-    ssh -i "%sshKeyPath%" %defUser%@%host% -p %port%
+    echo --in if 7--
+    echo ssh -o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no -i %sshKeyPath% %defUser%@%host% -p %port%"
+    ssh -o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no -i "%sshKeyPath%" %defUser%@%host% -p %port%
     exit /b 0
 )
 
@@ -150,98 +157,112 @@ if "%arg5%"=="" if "%arg6%"=="" if "%arg7%"=="" (
 
 rem 远程to远程 :xx  :yy
 if "%firstChar5%"==":" if "%firstChar6%"==":" if "%arg7%"=="" (
+    echo --in if 8--
     set "src=%arg5:~1%"
     set "dst=%arg6:~1%"
     echo scp -3: %defUser%@%host%:!src!  to  %defUser%@%host%:!dst!
-    scp -3 -i "%sshKeyPath%" -P %port% -r %defUser%@%host%:"!src!" %defUser%@%host%:"!dst!"
+    scp -3 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "%sshKeyPath%" -P %port% -r %defUser%@%host%:"!src!" %defUser%@%host%:"!dst!"
     exit /b 0
 )
 rem 远程to本地 :xx  yy:
 
 if "%firstChar5%"==":" if "%lastChar6%"==":" if "%arg7%"=="" (
+    echo --in if 9--
     echo %arg5%
     echo %arg6%
     set "src=%arg5:~1%"
     set "dst=%arg6:~0,-1%"
-    echo scp: %defUser%@%host%:!src!  to  !dst!
-    scp -i "%sshKeyPath%" -P %port% -r %defUser%@%host%:"!src!" "!dst!"
+    echo scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  %defUser%@%host%:!src!  to  !dst!
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "%sshKeyPath%" -P %port% -r %defUser%@%host%:"!src!" "!dst!"
     exit /b 0
 )
 
 rem 本地to远程 xx:  :yy
 if "%lastChar5%"==":" if "%firstChar6%"==":" if "%arg7%"=="" (
+    echo --in if 10--
     set "src=%arg5:~0,-1%"
     set "dst=%arg6:~1%"
     echo scp: !src!  to  %defUser%@%host%:!dst!
-    scp -i "%sshKeyPath%" -P %port% -r "!src!" %defUser%@%host%:"!dst!"
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "%sshKeyPath%" -P %port% -r "!src!" %defUser%@%host%:"!dst!"
     exit /b 0
 )
 rem 本地to本地 xx:  yy:
 if "%lastChar5%"==":" if "%lastChar6%"==":" if "%arg7%"=="" (
+    echo --in if 11--
     set "src=%arg5:~0,-1%"
     set "dst=%arg6:~0,-1%"
     echo scp: !src!  to  !dst!
-    scp -i "%sshKeyPath%" -P %port% -r "!src!" "!dst!"
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "%sshKeyPath%" -P %port% -r "!src!" "!dst!"
     exit /b 0
 )
 
 
 
 
-
+rem arg5 以 / 开头, 且 arg6/arg7 为空 to VSCode远程打开(默认用户 + 指定Key)
+if "%firstChar5%"=="/" if "%arg6%"=="" if "%arg7%"=="" (
+    echo --in if 12--
+    echo "VSCode open remote: %arg5%"
+    code --remote=ssh-remote+%defUser%@%host%:%port% %arg5%
+    exit /b 0
+)
+rem arg5 以 / 开头, arg6 不空, arg7空 to SFTP 同步(默认用户 + 指定Key)
+if "%firstChar5%"=="/" if not "%arg6%"=="" if not "%firstChar6%"==":" if "%arg7%"=="" (
+    echo --in if 13--
+    if not exist "%arg6%\.vscode" mkdir "%arg6%\.vscode"
+    if not exist "%arg6%\.vscode\SFTP.json" (
+        (
+        echo {
+        echo     "name": "%arg6:\=/%.%defUser%",
+        echo     "host": "%host%",
+        echo     "protocol": "sftp",
+        echo     "port": %port%,
+        echo     "username": "%defUser%",
+        echo     "privateKeyPath": "%sshKeyPath:\=/%",
+        echo     "remotePath": "%arg5%",
+        echo     "uploadOnSave": true,
+        echo     "useTempFile": false,
+        echo     "openSsh": false
+        echo }
+        ) > "%arg6%\.vscode\SFTP.json"
+    )
+    echo "SFTP已设置同步, 需要安装插件: SFTP by Natizyskunk"
+    code "%arg6%"
+    exit /b 0
+)
 rem arg5 以 / 结尾, 且 arg6/arg7 为空 to VSCode远程打开(默认用户 + 指定Key)
 if "%lastChar5%"=="/" if "%arg6%"=="" if "%arg7%"=="" (
+    echo --in if 14--
     echo "VSCode远程打开: %arg5%"
-    for /f "delims=" %%a in ('ssh -i "%sshKeyPath%" %defUser%@%host% -o ServerAliveInterval=60 -p %port% echo $HOME') do (
+    for /f "delims=" %%a in ('ssh -n -o "StrictHostKeyChecking=no" -i "%sshKeyPath%" %defUser%@%host% -o "ServerAliveInterval=60" -p %port% echo $HOME') do (
         set "opssshresult=%%a"
     )
+    if "!opssshresult!"=="" (
+        echo "获取对端主机的$HOME 目录失败. (对端主机未开机?安全组未开放端口?不是Linux?)"
+        exit /b 1
+    )
+    if "!opssshresult!"=="$HOME" (
+        echo "获取对端主机的$HOME 目录失败. (对端主机未开机?安全组未开放端口?不是Linux?)"
+        exit /b 1
+    )
+    echo code --remote=ssh-remote+%defUser%@%host%:%port% !opssshresult!/%arg5%
     code --remote=ssh-remote+%defUser%@%host%:%port% !opssshresult!/%arg5%
     exit /b 0
 )
 rem arg5 以 / 结尾, arg6 不空, arg7空 to SFTP 同步(默认用户 + 指定Key)
 if "%lastChar5%"=="/" if not "%arg6%"=="" if not "%firstChar6%"==":" if "%arg7%"=="" (
+    echo --in if 15--
     if not exist "%arg6%\.vscode" mkdir "%arg6%\.vscode"
+    echo "VSCode SFTP 远程打开: %arg5%"
     if not exist "%arg6%\.vscode\SFTP.json" (
         (
         echo {
-        echo     "name": "%arg6%.%defUser%",
+        echo     "name": "%arg6:\=/%.%defUser%",
         echo     "host": "%host%",
         echo     "protocol": "sftp",
         echo     "port": %port%,
         echo     "username": "%defUser%",
-        echo     "privateKeyPath": "%sshKeyPath%",
-        echo     "remotePath": "%arg5%",
-        echo     "uploadOnSave": true,
-        echo     "useTempFile": false,
-        echo     "openSsh": false
-        echo }
-        ) > "%arg6%\.vscode\SFTP.json"
-    )
-    echo "SFTP已设置同步, 需要安装插件: SFTP by Natizyskunk"
-    code "%arg6%"
-    exit /b 0
-)
-rem arg5 以 / 开头, 且 arg6/arg7 为空 to VSCode远程打开(默认用户 + 指定Key)
-if "%firstChar5%"=="/" if "%arg6%"=="" if "%arg7%"=="" (
-    echo "VSCode open remote: %arg5%"
-    for /f "delims=" %%a in ('ssh -i "%sshKeyPath%" %defUser%@%host% -o ServerAliveInterval=60 -p %port% echo $HOME') do (
-        set "opssshresult=%%a"
-    )
-    code --remote=ssh-remote+%defUser%@%host%:%port% !opssshresult!/%arg5%
-    exit /b 0
-)
-rem arg5 以 / 开头, arg6 不空, arg7空 to SFTP 同步(默认用户 + 指定Key)
-if "%firstChar5%"=="/" if not "%arg6%"=="" if not "%firstChar6%"==":" if "%arg7%"=="" (
-    if not exist "%arg6%\.vscode" mkdir "%arg6%\.vscode"
-    if not exist "%arg6%\.vscode\SFTP.json" (
-        (
-        echo {
-        echo     "name": "%arg6%.%defUser%",
-        echo     "host": "%host%",
-        echo     "protocol": "sftp",
-        echo     "port": %port%,
-        echo     "username": "%defUser%",
-        echo     "privateKeyPath": "%sshKeyPath%",
+        echo     "privateKeyPath": "%sshKeyPath:\=/%",
         echo     "remotePath": "%arg5%",
         echo     "uploadOnSave": true,
         echo     "useTempFile": false,
@@ -258,52 +279,55 @@ if "%firstChar5%"=="/" if not "%arg6%"=="" if not "%firstChar6%"==":" if "%arg7%
 
 
 
+rem %arg5% 为空，以及: /开头结尾的情况都处理完了，剩下即不为空且不以 : / 开头或结尾.
 
 
 
-
-
-rem %arg5% 为空，以及: /开通结尾的情况都处理完了，剩下即不为空且不以 : / 开头或结尾.
 
 
 rem %6 %7 都为空: 指定用户SSH登录.
 if "%arg6%"=="" if "%arg7%"=="" (
-    echo "指定用户SSH: %arg5%@%host%  -p %port%, key=%sshKeyPath%"
-    ssh -i "%sshKeyPath%" %arg5%@%host% -p %port%
+    echo --in if 16--
+    echo "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i \"%sshKeyPath%\" %arg5%@%host% -p %port%"
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "%sshKeyPath%" %arg5%@%host% -p %port%
     exit /b 0
 )
 
 
 rem 远程to远程 :xx  :yy
 if "%firstChar6%"==":" if "%firstChar7%"==":" (
+    echo --in if 17--
     set "src=%arg6:~1%"
     set "dst=%arg7:~1%"
     echo scp -3: %arg5%@%host%:!src!  to  %arg5%@%host%:!dst!
-    scp -3 -i "%sshKeyPath%" -P %port% -r %arg5%@%host%:"!src!" %arg5%@%host%:"!dst!"
+    scp -3 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "%sshKeyPath%" -P %port% -r %arg5%@%host%:"!src!" %arg5%@%host%:"!dst!"
     exit /b 0
 )
 rem 远程to本地 :xx  yy:
 if "%firstChar6%"==":" if "%lastChar7%"==":" (
+    echo --in if 18--
     set "src=%arg6:~1%"
     set "dst=%arg7:~0,-1%"
     echo scp: %arg5%@%host%:!src!  to  !dst!
-    scp -i "%sshKeyPath%" -P %port% -r %arg5%@%host%:"!src!" "!dst!"
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "%sshKeyPath%" -P %port% -r %arg5%@%host%:"!src!" "!dst!"
     exit /b 0
 )
 rem 本地to远程 xx:  :yy
 if "%lastChar6%"==":" if "%firstChar7%"==":" (
+    echo --in if 19--
     set "src=%arg6:~0,-1%"
     set "dst=%arg7:~1%"
     echo scp: !src!  to  %arg5%@%host%:!dst!
-    scp -i "%sshKeyPath%" -P %port% -r "!src!" %arg5%@%host%:"!dst!"
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "%sshKeyPath%" -P %port% -r "!src!" %arg5%@%host%:"!dst!"
     exit /b 0
 )
 rem 本地to本地 xx:  yy:
 if "%lastChar6%"==":" if "%lastChar7%"==":" (
+    echo --in if 20--
     set "src=%arg6:~0,-1%"
     set "dst=%arg7:~0,-1%"
     echo scp: !src!  to  !dst!
-    scp -i "%sshKeyPath%" -P %port% -r "!src!" "!dst!"
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "%sshKeyPath%" -P %port% -r "!src!" "!dst!"
     exit /b 0
 )
 
@@ -313,26 +337,45 @@ if "%lastChar6%"==":" if "%lastChar7%"==":" (
 
 
 rem 情况: 指定用户VSCode打开远程目录.
-if not "%lastChar6%"=="x" if "%arg7%"=="" (
+if "%firstChar6%"=="/" if "%arg7%"=="" (
+    echo --in if 21--
     echo "指定用户VSCode打开: %arg5%@%host%:%arg6%"
-    for /f "delims=" %%a in ('ssh -i "%sshKeyPath%" %arg5%@%host% -o ServerAliveInterval=60 -p %port% echo $HOME') do (
+    echo code --remote=ssh-remote+%arg5%@%host%:%port%  %arg6%
+    code --remote=ssh-remote+%arg5%@%host%:%port%  %arg6%
+    exit /b 0
+)
+rem 情况: 指定用户VSCode打开远程目录.
+if not "%lastChar6%"=="/" if "%arg7%"=="" (
+    echo --in if 22--
+    echo "指定用户VSCode打开: %arg5%@%host%:%arg6%"
+    for /f "delims=" %%a in ('ssh -n -o "StrictHostKeyChecking=no" -i "%sshKeyPath%" %arg5%@%host% -o "ServerAliveInterval=60" -p %port% echo $HOME') do (
         set "opssshresult=%%a"
     )
+    if "!opssshresult!"=="" (
+        echo "获取对端主机的$HOME 目录失败. (对端主机未开机?安全组未开放端口?不是Linux?)"
+        exit /b 1
+    )
+    if "!opssshresult!"=="$HOME" (
+        echo "获取对端主机的$HOME 目录失败. (对端主机未开机?安全组未开放端口?不是Linux?)"
+        exit /b 1
+    )
+    echo code --remote=ssh-remote+%arg5%@%host%:%port% !opssshresult!/%arg6%
     code --remote=ssh-remote+%arg5%@%host%:%port% !opssshresult!/%arg6%
     exit /b 0
 )
 rem 情况: 指定用户SFTP同步配置.
 if not "%lastChar6%"=="" if not "%arg7%"=="" if not "%firstChar7%"==":" (
+    echo --in if 23--
     if not exist "%arg7%\.vscode" mkdir "%arg7%\.vscode"
     if not exist "%arg7%\.vscode\SFTP.json" (
         (
         echo {
-        echo     "name": "%arg7%.%arg5%",
+        echo     "name": "%arg7:\=/%.%arg5%",
         echo     "host": "%host%",
         echo     "protocol": "sftp",
         echo     "port": %port%,
         echo     "username": "%arg5%",
-        echo     "privateKeyPath": "%sshKeyPath%",
+        echo     "privateKeyPath": "%sshKeyPath:\=/%",
         echo     "remotePath": "%arg6%",
         echo     "uploadOnSave": true,
         echo     "useTempFile": false,
